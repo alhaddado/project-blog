05b8f0a75241a9af7d2afdb89a0e9c0bc2b171ae
Omar Alhaddad <omar.alhaddad@kw.zain.com>
2024-06-04 12:20:55 +0300

    Exercise 1

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,18 @@ import React from 'react';
 import BlogSummaryCard from '@/components/BlogSummaryCard';
 
 import styles from './homepage.module.css';
+import { getBlogPostList } from "@/helpers/file-helpers"
 
-function Home() {
+import { BLOG_TITLE } from "@/constants"
+
+export const metadata = {
+  title: BLOG_TITLE,
+  name: "description",
+  content: "A wonderful blog about JavaScript"
+}
+
+async function Home() {
+  const list = await getBlogPostList();
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.mainHeading}>
@@ -12,12 +22,15 @@ function Home() {
       </h1>
 
       {/* TODO: Iterate over the data read from the file system! */}
-      <BlogSummaryCard
-        slug="example"
-        title="Hello world!"
-        abstract="This is a placeholder, an example which shows how the “BlogSummaryCard” component should be used. You'll want to swap this out based on the data from the various MDX files!"
-        publishedOn={new Date()}
-      />
+      {list.map((post) => (
+        <BlogSummaryCard
+          key={post.slug}
+          slug={post.slug}
+          title={post.title}
+          abstract={post.abstract}
+          publishedOn={post.publishedOn}
+        />
+      ))}
     </div>
   );
 }
