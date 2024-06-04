2e31179479f5407df5c42403060ba16e035ca45f
Omar Alhaddad <omar.alhaddad@kw.zain.com>
2024-06-04 12:21:24 +0300

    Exercise 2 + Others

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -3,21 +3,46 @@ import React from 'react';
 import BlogHero from '@/components/BlogHero';
 
 import styles from './postSlug.module.css';
+import { loadBlogPost } from "@/helpers/file-helpers"
+import { MDXRemote } from 'next-mdx-remote/rsc'
+import CodeSnippet from "@/components/CodeSnippet"
+import DivisionGroupsDemo from "@/components/DivisionGroupsDemo"
+import CircularColorsDemo from "@/components/CircularColorsDemo"
 
-function BlogPost() {
+export async function generateMetadata({ params: { postSlug }}) {
+  const post = await loadBlogPost(postSlug);
+
+  return {
+    title: post.frontmatter.title,
+    name: post.frontmatter.abstract,
+    content: post.content,
+  }
+}
+
+async function BlogPost({ params: { postSlug }}) {
+  const post = await loadBlogPost(postSlug);
+  const components = {
+    pre: (props) => (
+      <CodeSnippet {...props}>
+        {props.children}
+      </CodeSnippet>
+    ),
+    DivisionGroupsDemo: (props) => (
+      <DivisionGroupsDemo {...props} />
+    ),
+    CircularColorsDemo: (props) => (
+      <CircularColorsDemo {...props} />
+    ),
+  }
+
   return (
     <article className={styles.wrapper}>
       <BlogHero
-        title="Example post!"
-        publishedOn={new Date()}
+        title={post.frontmatter.title}
+        publishedOn={post.frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <p>This is where the blog post will go!</p>
-        <p>
-          You will need to use <em>MDX</em> to render all of
-          the elements created from the blog post in this
-          spot.
-        </p>
+        <MDXRemote source={post.content} components={{...components}} />
       </div>
     </article>
   );
